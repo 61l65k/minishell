d215ba96113078b7ced71bf13235b632847cd095
alex <alexxpyykonen@gmail.com>
2024-02-25 21:22:48 +0200

    resolve confilcts

--- srcs/signals.c
+++ srcs/signals.c
@@ -6,11 +6,7 @@
 /*   By: ttakala <ttakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 21:56:23 by apyykone          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2024/02/25 06:06:43 by apyykone         ###   ########.fr       */
-=======
 /*   Updated: 2024/02/25 12:05:17 by ttakala          ###   ########.fr       */
->>>>>>> eaca9d0ab313a9e61b4708c6831f1c71b6189654
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +31,6 @@ static void	ft_signal_handler(int signo, siginfo_t *info, void *context)
 	(void)context;
 	(void)info;
 	if (signo == SIGINT)
-<<<<<<< HEAD
 	{
 		write(1, "\n", 1);
 		rl_replace_line("", 0);
@@ -44,13 +39,6 @@ static void	ft_signal_handler(int signo, siginfo_t *info, void *context)
 	}
 	else if (signo == SIGQUIT)
 		printf("\n");
-=======
-		printf("\n");
-	else if (signo == SIGUSR1)
-		printf("Received SIGUSR1 %d\n", info->si_pid);
-	else if (signo == SIGQUIT)
-		return ;
->>>>>>> eaca9d0ab313a9e61b4708c6831f1c71b6189654
 	else
 		printf("Unknown signal\n");
 }
