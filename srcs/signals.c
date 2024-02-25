d8a3eabaacdac05ee642fc23af4c4abf6a215af2
ttakala <ttakala@student.hive.fi>
2024-02-25 12:15:33 +0200

    make it compile with -Werror (printf was being called with an empty string)

--- srcs/signals.c
+++ srcs/signals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apyykone <apyykone@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: ttakala <ttakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 21:56:23 by apyykone          #+#    #+#             */
-/*   Updated: 2024/02/21 21:56:24 by apyykone         ###   ########.fr       */
+/*   Updated: 2024/02/25 12:05:17 by ttakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ void	ft_signal_handler(int signo, siginfo_t *info, void *context)
 {
 	(void)context;
 	if (signo == SIGINT)
-		ft_printf("\n");
+		printf("\n");
 	else if (signo == SIGUSR1)
-		ft_printf("Received SIGUSR1 %d\n", info->si_pid);
+		printf("Received SIGUSR1 %d\n", info->si_pid);
 	else if (signo == SIGQUIT)
-		printf("");
+		return ;
 	else
-		printf("Uknown signal\n");
+		printf("Unknown signal\n");
 }
 
 int	init_signals(void)
