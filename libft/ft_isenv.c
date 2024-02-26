0abc300ab746744f7546fcf1827d1f819adb5fb8
alex <alexxpyykonen@gmail.com>
2024-02-26 18:30:20 +0200

    new libft function ft_isenv() checks that current character is valid env variable character

--- libft/ft_isenv.c
+++ libft/ft_isenv.c
@@ -1,20 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*   ft_isenv.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: apyykone <apyykone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/23 15:13:53 by apyykone          #+#    #+#             */
-/*   Updated: 2023/10/26 15:25:00 by apyykone         ###   ########.fr       */
+/*   Created: 2024/02/26 18:28:46 by apyykone          #+#    #+#             */
+/*   Updated: 2024/02/26 18:28:47 by apyykone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
+/**
+ * @brief Checks if the character is a valid environment variable character.
+ * & Returns 1 if it is, otherwise 0.
+ */
 int	ft_isenv_var(int c)
 {
-	if ((c >= 48 && c <= 57) || (c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	if ((c >= 48 && c <= 57) || (c >= 65 && c <= 90) || (c >= 97 && c <= 122)
+		|| c == '_')
 		return (1);
 	return (0);
 }
