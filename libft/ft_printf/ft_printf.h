c80b7299516febc9966f019651d9cbc53c666a92
ttakala <ttakala@student.hive.fi>
2024-02-27 10:55:51 +0200

    libft makefile add ft_strncat

--- libft/ft_printf/ft_printf.h
+++ libft/ft_printf/ft_printf.h
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_fprintf.h                                       :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ttakala <ttakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 10:03:54 by ttakala           #+#    #+#             */
-/*   Updated: 2024/02/26 18:39:03 by ttakala          ###   ########.fr       */
+/*   Updated: 2024/02/27 10:53:43 by ttakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-int		ft_printf(const char *format, ...);
-int		ft_fprintf(int output_fd, const char *format, ...);
+int	ft_printf(const char *format, ...);
+int	ft_fprintf(int output_fd, const char *format, ...);
 
 #endif
