f7d5650d490d31ddbaa3e48189547b7459355953
ttakala <ttakala@student.hive.fi>
2024-03-11 14:59:40 +0200

    Figured out how to add color to the prompt without word wrap breaking. Might have to check this again on Mac to make sure it works there too

--- srcs/main.c
+++ srcs/main.c
@@ -18,14 +18,16 @@
  */
 static void	ft_takeinput(t_shellstate *state)
 {
-	static char	prompt[PATH_MAX + 4];
+	static char	prompt[PATH_MAX + sizeof(DBLUE_E) + sizeof(RESET_E"$ ") + 1];
+	size_t		len;
 
-	if (getcwd(prompt, PATH_MAX) == NULL)
+	len = ft_strlcpy(prompt, DBLUE_E, sizeof(prompt));
+	if (getcwd(prompt + len, PATH_MAX) == NULL)
 		ft_free_exit(state, ERR_GETCWD, EXIT_FAILURE);
-	ft_strlcat(prompt, "\% ", sizeof(prompt));
+	ft_strncat(prompt, RESET_E"$ ", sizeof(prompt));
 	state->input_string = readline(prompt);
 	if (state->input_string == NULL)
-		ft_free_exit(state, NULL, EXIT_FAILURE);
+		ft_free_exit(state, ERR_READLINE, EXIT_FAILURE);
 	if (ft_strlen(state->input_string))
 		add_history(state->input_string);
 }
