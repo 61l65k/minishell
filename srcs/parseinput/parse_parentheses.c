ac97adca91b9484986b771f57c59d7eba73fb531
alex <alexxpyykonen@gmail.com>
2024-03-13 06:11:02 +0200

    fix for parentheses check

--- srcs/parseinput/parse_parentheses.c
+++ srcs/parseinput/parse_parentheses.c
@@ -31,23 +31,23 @@ int	adjust_index(const char *str, int i, bool adjust_next)
 
 int	check_parentheses(t_operatorhelper *op, t_shellstate *s)
 {
-	const char	current_char = s->input_string[op->i];
+	const char	*current_char = &s->input_string[op->i];
 	const int	prev_index = adjust_index(s->input_string, op->i - 1, false);
 	const int	next_index = adjust_index(s->input_string, op->i + 1, true);
 
-	if (current_char == '(')
+	if (*current_char == '(')
 	{
 		if (op->i != 0 && prev_index >= 0 && check_for_op(op, s,
-				prev_index) == OP_NONE)
+				prev_index) == OP_NONE && *(current_char - 1) != '(')
 			return (print_syntax_err("(", NULL), FAILURE);
 		op->paren_depth++;
 	}
-	else if (current_char == ')')
+	else if (*current_char == ')')
 	{
 		if (op->paren_depth <= 0)
 			return (print_syntax_err(")", NULL), FAILURE);
 		if (s->input_string[next_index] != '\0' && check_for_op(op, s,
-				next_index) == OP_NONE)
+				next_index) == OP_NONE && *(current_char + 1) != ')')
 			return (print_syntax_err(")", NULL), FAILURE);
 		op->paren_depth--;
 	}
