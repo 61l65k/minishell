077f2777f8aff85388906d6fa34d6af27b3193af
ttakala <ttakala@student.hive.fi>
2024-03-14 12:19:13 +0200

    remove unused #defines

--- includes/minimessages.h
+++ includes/minimessages.h
@@ -12,24 +12,11 @@
 
 #ifndef MINIMESSAGES_H
 # define MINIMESSAGES_H
-# define CLEAR_SCREEN "\033[H\033[J"
-# define CYAN "\033[0;36m"
-# define RESET "\033[0m"
-# define RED "\033[0;31m"
-# define GREEN "\033[0;32m"
-# define CYAN_E "\001\033[0;36m\002"
 # define RESET_E "\001\033[0m\002"
-# define RED_E "\001\033[0;31m\002"
-# define GREEN_E "\001\033[0;32m\002"
-# define BLUE_E "\001\033[0;34m\002"
 # define DBLUE_E "\001\033[0;94m\002"
-# define ERR_READLINE "\033[0;31mError: readline() failed \n\033[0m"
-# define ERR_PROCESTRING "\033[0;31mError: processing string\n\033[0m"
-# define ERR_EXECV "\033[0;31mError: execv() failed\n\033[0m"
 # define ERR_GETCWD "\033[0;31mError: getcwd() failed \n\033[0m"
 # define ERR_PIPE "\033[0;31mError: pipe() failed\n \033[0m"
 # define ERR_FORK "\033[0;31mError: fork() failed\n \033[0m"
-# define ERR_SIGACTION "\033[0;31mError: sigaction() failed\n\033[0m"
 # define ERR_QUOTES "\033[0;31mminishell: quotes not closed\n\033[0m"
 # define ERR_MALLOC "\033[0;31mError: malloc() failed\n\033[0m"
 # define ERR_PARENTHESES "\033[0;31mminishell: parentheses not closed\n\033[0m"
@@ -40,11 +27,7 @@ delimited by end-of-file (wanted `%s')\n"
 # define ERR_HEREDOC_DELIMITER \
 	"minishell: syntax error near unexpected \
 token `newline'\n"
-# define RM_MESSAGE \
-	"Are you sure you want to\
- remove all matched with wildcard? (y/n)\n"
 # define SIGINT_EXIT 130
-# define CTRLD_EXIT 69
 
 typedef enum e_charflag_bits
 {
