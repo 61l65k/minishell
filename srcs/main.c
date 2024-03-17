442c3865b2b044770913a19d2d19b904b4866b81
ttakala <ttakala@student.hive.fi>
2024-03-17 12:32:58 +0200

    remove OLDPWD only when in interactive mode

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ttakala <ttakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 12:17:20 by apyykone          #+#    #+#             */
-/*   Updated: 2024/03/15 22:24:23 by ttakala          ###   ########.fr       */
+/*   Updated: 2024/03/17 12:31:43 by ttakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,8 @@ int	main(int argc, char **argv, const char **envp)
 	vec_new(&state.pid, 10, sizeof(pid_t));
 	if (!state.envp || state.pid.memory == NULL)
 		ft_free_exit(&state, ERR_MALLOC, EXIT_FAILURE);
-	remove_env_var("OLDPWD", &state);
+	if (isatty(STDIN_FILENO))
+		remove_env_var("OLDPWD", &state);
 	while (1)
 	{
 		ft_free_resets(&state);
