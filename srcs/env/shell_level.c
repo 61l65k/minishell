75f29fdcbceae15e69934fa78b4c22640bc6827c
ttakala <ttakala@student.hive.fi>
2024-03-19 23:28:18 +0200

    make SHLVL error handling same as bash

--- srcs/env/shell_level.c
+++ srcs/env/shell_level.c
@@ -6,7 +6,7 @@
 /*   By: ttakala <ttakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 21:53:01 by ttakala           #+#    #+#             */
-/*   Updated: 2024/03/19 22:23:08 by ttakala          ###   ########.fr       */
+/*   Updated: 2024/03/19 23:24:23 by ttakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,12 @@ int	get_shell_level(char *const envp[])
 {
 	const char	*shlvl_str = ft_getenv("SHLVL", envp);
 	int			shell_level;
-	int			overflow;
 
 	if (shlvl_str == NULL)
-		return (-1);
+		return (0);
 	if (!is_numeric(shlvl_str))
-		return (-1);
-	shell_level = str_to_long(shlvl_str, &overflow);
-	if (overflow || shell_level < 0)
-		return (-1);
+		return (0);
+	shell_level = ft_atoi(shlvl_str);
 	return (shell_level);
 }
 
@@ -34,10 +31,16 @@ void	update_shell_level(t_shellstate *state)
 	int			new_shlvl;
 	char		*new_shlvl_str;
 
-	if (prev_shlvl < 0)
+	new_shlvl = prev_shlvl + 1;
+	if (new_shlvl > 999)
+	{
+		ft_fprintf(STDERR_FILENO,
+			"minishell: warning: shell level (%d) too high, resetting to 1\n",
+			new_shlvl);
 		new_shlvl = 1;
-	else
-		new_shlvl = prev_shlvl + 1;
+	}
+	else if (new_shlvl < 0)
+		new_shlvl = 0;
 	new_shlvl_str = ft_itoa(new_shlvl);
 	if (new_shlvl_str == NULL)
 		ft_free_exit(state, ERR_MALLOC, 1);
